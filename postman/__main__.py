a09d361ad282d69fc7bdb130bca3febc4bebd823
Graham Poulter <graham.poulter@gmail.com>
2011-09-01 16:43:08 +0200

    send: fixed error reporting, show_stats: fixed display bugs, use terse formatting. General: use print() and .format()

--- postman/__main__.py
+++ postman/__main__.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import argparse
 import sys
 
@@ -6,119 +8,113 @@ import boto
 from postman import __version__
 
 
-def out(msg, args):
-    if args.verbose:
-        sys.stdout.write("%s\n" % msg)
-        sys.stdout.flush()
-
-
 def cmd_send(args):
     ses = boto.connect_ses()
-    out("Sending mail to: %s" % ", ".join(args.destinations), args)
     msg = sys.stdin.read()
-    r = ses.send_raw_email(msg, args.f, args.destinations)
-    if r.get("SendRawEmailResponse", {}).get("SendRawEmailResult", {}).get("MessageId"):
-        out("OK", args)
+    details = 'ses_from={0} {1}'.format(args.f, ' '.join(
+        'ses_to=' + d for d in args.destinations))
+    if args.f == "MAILER-DAEMON":
+        print("ses_status=IGNORE {0}".format(details))
     else:
-        out("ERROR: %s" % r, args)
+        try:
+            result = ses.send_raw_email(msg, args.f, args.destinations)
+            if result.get("SendRawEmailResponse", {})\
+               .get("SendRawEmailResult", {})\
+               .get("MessageId"):
+                print("ses_status=OK {0}".format(details))
+            else:
+                print("ses_status=NOTSENT {0} {1}".format(details, result))
+                sys.exit(1)
+        except boto.exception.BotoServerError as err:
+            print('ses_status=ERROR status={0} reason="{1}"'
+                  ' message="{2}" code={3} {4}'.format(
+                      err.status, err.reason, err.error_message,
+                      err.error_code, details))
+            sys.exit(1)
 
 
 def cmd_verify(args):
     ses = boto.connect_ses()
     for email in args.email:
         ses.verify_email_address(email)
-        out("Verification for %s sent." % email, args)
+        print("Verification for {0} sent.".format(email))
 
 
-def cmd_list_verified(args):
+def cmd_list_verified(_args):
     ses = boto.connect_ses()
-    args.verbose = True
-    
     addresses = ses.list_verified_email_addresses()
     addresses = addresses["ListVerifiedEmailAddressesResponse"]
     addresses = addresses["ListVerifiedEmailAddressesResult"]
     addresses = addresses["VerifiedEmailAddresses"]
-    
     if not addresses:
-        out("No addresses are verified on this account.", args)
+        print("No addresses are verified on this account.")
         return
-    
-    for address in addresses:
-        out(address, args)
+    for address in sorted(addresses):
+        print(address)
 
 
-def cmd_show_quota(args):
+def cmd_show_quota(_args):
     ses = boto.connect_ses()
-    args.verbose= True
-    
     data = ses.get_send_quota()["GetSendQuotaResponse"]["GetSendQuotaResult"]
-    out("Max 24 Hour Send: %s" % data["Max24HourSend"], args)
-    out("Sent Last 24 Hours: %s" % data["SentLast24Hours"], args)
-    out("Max Send Rate: %s" % data["MaxSendRate"], args)
+    print("Max 24 Hour Send: {0}".format(data["Max24HourSend"]))
+    print("Sent Last 24 Hours: {0}".format(data["SentLast24Hours"]))
+    print("Max Send Rate: {0}".format(data["MaxSendRate"]))
 
 
-def cmd_show_stats(args):
+def cmd_show_stats(_args):
     ses = boto.connect_ses()
-    args.verbose = True
-    
     data = ses.get_send_statistics()
     data = data["GetSendStatisticsResponse"]["GetSendStatisticsResult"]
-    for datum in data["SendDataPoints"]:
-        out("Complaints: %s" % datum["Complaints"], args)
-        out("Timestamp: %s" % datum["Timestamp"], args)
-        out("DeliveryAttempts: %s" % datum["DeliveryAttempts"], args)
-        out("Bounces: %s" % datum["Bounces"], args)
-        out("Rejects: %s" % datum["Rejects"], args)
-        out("", args)
+    fmt = "{0:>20} {1:>10} {2:>8} {3:>7} {4:>7}"
+    print(fmt.format('Timestamp', 'Complaints', 'Attempts', 'Bounces', 'Rejects'))
+    for datum in sorted(data["SendDataPoints"], key=lambda x:x.get('Timestamp')):
+        print(fmt.format(datum["Timestamp"],
+                         datum["Complaints"],
+                         datum["DeliveryAttempts"],
+                         datum["Bounces"],
+                         datum["Rejects"]))
 
 
 def cmd_delete_verified(args):
     ses = boto.connect_ses()
     for email in args.email:
         ses.delete_verified_email_address(email_address=email)
-        out("Deleted %s" % email, args)
+        print("Deleted {0}".format(email))
 
 
 def main():
-    parser = argparse.ArgumentParser(prog="postman", description="send an email via Amazon SES")
-    parser.add_argument("--version", action="version", version="%%(prog)s %s" % __version__)
-    parser.add_argument("--verbose", action="store_true")
-    
-    command_parsers = parser.add_subparsers(dest="command")
-    
-    # cmd: send
-    parser_send = command_parsers.add_parser("send")
+    parser = argparse.ArgumentParser(
+        prog="postman", description="send an email via Amazon SES")
+    parser.add_argument("--version", action="version",
+                        version="postman " + __version__)
+
+    commands = parser.add_subparsers(dest="command")
+
+    parser_send = commands.add_parser("send")
     parser_send.add_argument("-f",
         help="the address to send the message from, must be validated")
-    parser_send.add_argument("destinations", metavar="TO", type=str, nargs="+",
+    parser_send.add_argument("destinations", metavar="TO", nargs="+",
         help="a list of email addresses to deliver message to")
-    
-    # cmd: verify
-    parser_send = command_parsers.add_parser("verify")
-    parser_send.add_argument("email", nargs="+",
+
+    parser_verify = commands.add_parser("verify")
+    parser_verify.add_argument("email", nargs="+",
         help="an email address to verify for sending from")
-    
-    # cmd: list_verified
-    command_parsers.add_parser("list_verified")
-    
-    # cmd: show_quota
-    command_parsers.add_parser("show_quota")
-    
-    # cmd: show_stats
-    command_parsers.add_parser("show_stats")
-    
-    # cmd: delete_verified
-    parser_delete = command_parsers.add_parser("delete_verified")
+
+    commands.add_parser("list_verified")
+    commands.add_parser("show_quota")
+    commands.add_parser("show_stats")
+
+    parser_delete = commands.add_parser("delete_verified")
     parser_delete.add_argument("email", nargs="+",
-        help="verified email addresses that will be deleted from verification list")
-    
-    args = parser.parse_args()
-    
-    {
+        help="verified email addresses to deleted from verification list")
+
+    cmdmap = {
         "send": cmd_send,
         "verify": cmd_verify,
         "list_verified": cmd_list_verified,
         "show_quota": cmd_show_quota,
         "show_stats": cmd_show_stats,
         "delete_verified": cmd_delete_verified
-    }[args.command](args)
+    }
+    args = parser.parse_args()
+    cmdmap[args.command](args)
