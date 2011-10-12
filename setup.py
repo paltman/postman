556230e37f2c61ac3865ce8cb9d1a994f6df4b71
Graham Poulter <graham.poulter@gmail.com>
2011-10-12 15:56:40 +0200

    Make argparse requirement conditional on Python version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from setuptools import setup, find_packages
 
 from postman import __version__
@@ -7,6 +8,10 @@ from postman import __version__
 def read(*path):
     return open(os.path.join(os.path.abspath(os.path.dirname(__file__)), *path)).read()
 
+pyversion = sys.version_info[:2]
+install_requires = ["boto>=2.0"]
+if pyversion < (2,7) or (3,0) <= pyversion <= (3,1):
+    install_requires.append('argparse')
 
 setup(
     name = "postman",
@@ -22,8 +27,5 @@ setup(
             "postman = postman.__main__:main",
         ],
     },
-    install_requires = [
-        "argparse==1.2.1",
-        "boto==2.0"
-    ]
+    install_requires = install_requires,
 )
